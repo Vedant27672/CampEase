2744639def0e96986d96953ff38f181c50faf1be
Vedant Singh Chauhan <167565447+Vedant27672@users.noreply.github.com>
2025-11-02 11:21:01 +0530

    improved UI and dummy data

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const User = require('../models/user');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect('mongodb://localhost:27017/CampEase', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -21,14 +22,18 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
+    const user = new User({ email: 'testuser@example.com', username: 'testuser' });
+    await user.setPassword('password');
+    await user.save();
+
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '6816e9d8cbe3f65ee8f015d9',
+            author: user._id,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
+            images: [{ url: `https://picsum.photos/400?random=${Math.random()}`, filename: `campground-${i}` }],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
